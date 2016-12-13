be71d5181c935cfb8c6199ffdc0662e7fdb0c568
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-12-12 18:37:12 -0700

    Add tests for ConditionalTransformer fallback.

--- tests/ConditionalTransformerTest.php
+++ tests/ConditionalTransformerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use SSNepenthe\ColorUtils\Color;
+use SSNepenthe\ColorUtils\Transformers\Darken;
 use SSNepenthe\ColorUtils\Transformers\Lighten;
 use SSNepenthe\ColorUtils\Transformers\TransformerInterface;
 use SSNepenthe\ColorUtils\Transformers\ConditionalTransformer;
@@ -32,5 +33,22 @@ class ConditionalTransformerTest extends PHPUnit_Framework_TestCase
         $newColor = $transformer->transform($color);
 
         $this->assertNotSame($color, $newColor);
+    }
+
+    public function test_it_can_apply_a_fallback_transformation()
+    {
+        $transformer = new ConditionalTransformer(function(Color $color) : bool {
+            return $color->looksDark();
+        }, new Lighten(30), new Darken(30));
+
+        $color = Color::fromKeyword('red');
+        $newColor = $transformer->transform($color);
+
+        $this->assertEquals([102, 0, 0], $newColor->toArray()); // Darkened.
+
+        $color = Color::fromKeyword('green');
+        $newColor = $transformer->transform($color);
+
+        $this->assertEquals([26, 255, 26], $newColor->toArray()); // Lightened.
     }
 }
